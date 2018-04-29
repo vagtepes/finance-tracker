6c60e6cc1e42c8df45b6e0d6f650539d7ff70e3a
Evangelos <etzintsopoulos@gmail.com>
2018-04-29 18:06:22 +0100

    Finished the project

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,38 @@ class UsersController < ApplicationController
   end
   
   def my_friends
-	
+	@friendships = current_user.friends
   end
+  
+  def search
+	if params[:search_param].blank?
+	  flash.now[:danger]="You have enter an empty search string "
+	else
+	  @users = User.search(params[:search_param])
+	  @users = current_user.except_current_user(@users)
+	  flash.now[:danger]="No users matched this search criteria " if @users.blank?
+	end
+	respond_to do |format|
+	  format.js {render partial: 'friends/result'}
+	end
+	
+	def add_friend
+	  @friend= User.find(params[:friend])
+	  current_user.friendships.build(friend_id: @friend.id)
+	  
+	  if current_user.save
+		flash[:succcess]="Friend was successfully added"
+	  else
+		flash[:danger]="There was something wrong with the friend request"
+	  end
+	  redirect_to my_friends_path
+	end
+  end
+  
+  def show
+	@user = User.find(params[:id])
+	@user_stocks=@user.stocks
+  end
+  
+  
 end
